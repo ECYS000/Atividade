b2c4e2733698c02b6d618aae680e46c71821ac5e
ECYS000 <eversonc39@gmail.com>
2023-02-08 11:01:46 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 //Atividade 
 // 1 - Selecionassem um elemento 
-
-let teste1 = document.getElementsByClassName('testeP')
-console.log(teste1)
-
 // 2 - Alterar o conteúdo de um elemento
-
-let teste2 = document.querySelector('.testeAlterar')
-teste2.textContent = "Atividade concluído"
-
 // 3 - Removerem um elemento
-
-let teste3 = document.querySelector (' .testeAlterar')
-teste3.remove()
-
 // 4 - Desafio! Criar um botão que mostre algo na tela
 
-function teste4 (){
-    alert("Obrigado pela atenção")
+let texto = document.querySelector(' .texto1' );
+
+function traducao() {
+    texto.innerHTML = "Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galera de tipos e os embaralhou para fazer um livro de espécimes de tipos. Ele sobreviveu não apenas a cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Foi popularizado na década de 1960 com o lançamento de folhas Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker, incluindo versões de Lorem Ipsum.";
+}
+let removeTraducao = texto;
+function apagar() {
+
+removeTraducao.remove()}
+
+function ok(){
+    alert("Atividade tudo esta ok")
 }
